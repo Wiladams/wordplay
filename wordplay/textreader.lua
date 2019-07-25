357471b3d33599d04a9c0a506a449b075378cb50
William Adams <william_a_adams@msn.com>
2019-07-25 07:00:07 -0700

    redo readSingleLine

--- wordplay/textreader.lua
+++ wordplay/textreader.lua
@@ -20,57 +20,56 @@ function TextReader.new(self, src)
 end
 
 --[[
-    readLine
+    readSingleLine(src)
 
-    Return a single line as a lua string.  A 'line' is delimited
-    with 'cr/lf', or 'lf'
+    Parameters:
+        src - an octetstream positioned at the start of a line
+
+    Return a single line as a lua string.  
+    If src isEOF() then nil is returned
+    
+    A 'line' is delimited either with the combination of 'cr/lf', 
+    or a single 'lf'.  
+    
+    If there are individual 'cr' characters that are not followed
+    immediately by 'lf', they will be embedded in the string.
 
     Meaningfully, we don't include the line ending in the string.
 ]]
 local CR = string.byte('\r')
 local LF = string.byte('\n')
 
+
 local function readSingleLine(src)
-    if src:isEOF() then
-        return nil;
-    end
+    if src:isEOF() then return nil end
 
     local starting = src:tell();
-    local startPtr = src:getPositionPointer();
     local ending = starting
-
-    local haveCR = false;
-    local haveLF = false;
+    local startPtr = src:getPositionPointer();
 
     while not src:isEOF() do
-        local c = src:readOctet();
-        if haveCR then
-            if c == LF then
-                haveLF = true;
-                break;
-            elseif c ~= CR then
-                haveCR = false
-            end
-        else
-            if c == CR then
-                haveCR = true;
-            elseif c == LF then
-                haveLF = true;
-                break;
-            end
+        local c = src:peekOctet();
+        if c < 0 then
+            break;
         end
-    end
 
-    ending = src:tell();
-    if haveLF then
-        ending = ending - 1;
-    end
-    if haveCR then
-        ending = ending - 1;
+        if c == CR then
+            c = src:peekOctet(1);
+            if c == LF then
+                ending = src:tell();
+                src:skip(2)
+                break;
+            end
+        elseif c == LF then
+            src:skip(1)
+            break;
+        end
+
+        src:skip(1)
+        ending = src:tell()
     end
 
     local len = ending - starting
-    --print("LEN: ", len)
     local value = ffi.string(startPtr, len)
 
     return value;
